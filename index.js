0c2dde96dfe389cb3f34b4cc30a1d54d2d437267
Ania <aniaa.pawlik@gmail.com>
2023-10-18 01:44:53 +0200

    task implementation

--- index.js
+++ index.js
@@ -1,18 +1,134 @@
-function reveal() {
-    var reveals = document.querySelectorAll(".reveal");
-  
-    for (var i = 0; i < reveals.length; i++) {
-      var windowHeight = window.innerHeight;
-      var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = 150;
-  
-      if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add("active");
-      } else {
-        reveals[i].classList.remove("active");
-      }
-    }
+var translations = {
+  'en': {
+      'team': 'Team',
+      'history': 'history',
+      'offer': 'offer',
+      'opinion': 'opinion',
+      'contact': 'contact',
+      'nameTitle': 'We are Here to Help',
+      'explore': 'explore',
+      'practiceHistory': 'PRACTICE HISTORY',
+      'practiceHistoryText1': '"Liberty Legal Solutions" was founded in the early 20th century by two visionary attorneys, William Harrison and Emily Sinclair. Inspired by the social and political changes of the time, they established the firm in the heart of a bustling city. ',
+      'practiceHistoryText2': 'During its early years, the firm played a significant role in advocating for civil rights, women s suffrage, and labor rights. Their dedication to these causes earned them a reputation as champions of justice and liberty. The firm s commitment to defending the marginalized and oppressed communities in the face of adversity was unwavering.',
+      'practiceHistoryText3': 'Over the decades, "Liberty Legal Solutions" continued to evolve, adapting to the changing legal landscape while remaining true to its founding principles. The firm s attorneys took on landmark cases that shaped legal precedents and influenced societal progress.',
+      'practiceHistoryText4': 'Today, "Liberty Legal Solutions" stands as a respected institution with a legacy of defending civil liberties, championing social justice, and providing legal solutions that have a lasting impact on the community it serves. It remains committed to its historical mission of upholding liberty and justice for all.',
+      '100yearsExperience': 'More Than 100 Years of Experience',
+      'historyDate': 'History of "Liberty Legal Solutions"',
+      '1920': 'The law firm "Liberty Legal Solutions" was founded',
+      '1945': '"Liberty Legal Solutions" takes on its first defending a group of civil rights activists ',
+      '1980': 'The firm expands its practice areas to include labor law, environmental law, and consumer protection.',
+      '2000': 'Establishes a pro bono division dedicated to providing legal aid to underserved communities.',
+      '2010': 'The firm celebrates its 90th anniversary and reaffirms its dedication to upholding the principles of justice and liberty.',
+      'ourTeam': 'Our Team',
+      'William': 'William Harrison is a seasoned attorney. With over three decades of experience in the legal field, he is a respected figure in the legal community and a key driving force behind the firm s success. Mr. Harrison specializes in civil rights law, constitutional law, and complex litigation. His in-depth knowledge of these areas has earned him a reputation as a formidable advocate for justice and civil liberties. He has successfully represented clients in high-profile cases that have had a lasting impact on legal precedents.',
+      'Emily': 'Emily Sinclair is a highly accomplished attorney and serves as the Managing Partner of "Liberty Legal Solutions." With a career spanning more than two decades, she has earned a reputation as a legal powerhouse and a trailblazer in the field. Ms. Sinclair specializes in family law, particularly in cases involving complex divorce proceedings, child custody disputes, and spousal support negotiations. Her extensive experience and deep understanding of family dynamics make her a sought-after advocate for her clients.',
+      'services': 'Services',
+      'offer': 'Offer',
+      'laborLaw': 'labor law',
+      'laborLawText': 'Are you facing labor-related challenges or seeking expert advice on employment matters? "Liberty Legal Solutions" is here to support you. For a limited time, we are offering a free, confidential consultation with our experienced labor law attorneys. Our team specializes in employment contracts, workplace discrimination, wage disputes, wrongful termination, and more. We are committed to swift, effective resolution while upholding your rights.',
+      'civilLaw': 'civil law',
+      'civilLawText': 'Are you facing civil legal challenges and seeking expert guidance? "Liberty Legal Solutions" is here to assist you. For a limited time, we are pleased to offer a free, confidential consultation with our experienced civil law attorneys. Our team specializes in a wide range of civil law matters, including personal injury, contract disputes, property disputes, and more. We are dedicated to providing you with personalized legal solutions that address your unique needs and objectives.',
+      'administrativeLaw': 'administrative law',
+      'administrativeLawText': 'Are you facing administrative legal challenges and seeking expert guidance? "Liberty Legal Solutions" is here to assist you. For a limited time, we are pleased to offer a free, confidential consultation with our experienced administrative law attorneys. Our team specializes in various administrative law matters, including regulatory compliance, government agency interactions, and administrative hearings. We are dedicated to providing you with personalized legal solutions that navigate the complexities of administrative processes.',
+      'carAccidents': 'Car Accidents',
+      'motorcycleAccidents': 'Motorcycle Accidents',
+      'oilRigInjury': 'Oil Rig Injury',
+      'slipAndFall': 'Slip and Fall',
+      'constructionAccidents': 'Construction Accidents',
+      'uberAccidents': 'Uber Accidents',
+      'workInjuries': 'Work Injuries',
+      'bicycleAccidents': 'Bicycle Accidents',
+      'opinion1': 'Car Accident Case',
+      'opinion1Text': 'I was in a terrible car wreck back in June. I found Liberty Legal Solutions and was put into contact with Emily. She assisted me in finding and receiving medical treatment to treat my injuries so that I could return to work. I could not imagine having a better attorney for this situation.',
+      'opinion2': 'Child Support Case',
+      'opinion2Text': 'Liberty Legal Solutions did an amazing job on my case. They were able to resolve my family issue and allow me to see my children on a weekly basis. I would highly recommend this firm for any family law situation.',
+      'opinion3': 'Slip and Fall Case',
+      'opinion3Text': 'If you live in Dallas and need a personal injury lawyer, I could not recommend Liberty Legal Solutions more! Professional, communicative, and hardworking, I never anticipated feeling so cared for and heard by a lawyer!',
+      'contactText': 'The internet can t answer all your questions, turn to the experts instead. Contact our team to speak with one of our attorneys and learn how we can help you.',
+      'contactButton': 'Contact us',
+      'week': 'Monday - Friday:',
+      'hours': '9:00 AM - 5:00 PM',
+      'weekend': 'Saturday, Sunday: ',
+      'closed': 'Closed',
+      'questions': 'Any questions?',
+      'send': 'send',
+  },
+  'pl': {
+      'team': 'Zespół',
+      'history': 'Historia',
+      'offer': 'Oferta',
+      'opinion': 'Opinie',
+      'contact': 'Kontakt',
+      'nameTitle': 'Jesteśmy tu, żeby Ci pomóc',
+      'explore': 'zobacz',
+      'practiceHistory': 'HISTORIA PRAKTYKI',
+      'practiceHistoryText1': '"Liberty Legal Solutions" została założona na początku XX wieku przez dwóch prawników-wizjonerów, Williama Harrisona i Emily Sinclair. Zainspirowani przemianami społecznymi i politycznymi tamtych czasów, założyli firmę w sercu tętniącego życiem miasta.',
+      'practiceHistoryText2': 'Na początku swojej działalności firma odegrała znaczącą rolę w propagowaniu praw obywatelskich, prawa wyborczego kobiet i praw pracowniczych. Ich zaangażowanie w te sprawy przyniosło im reputację mistrzów sprawiedliwości i wolności. Zaangażowanie firmy w obronę marginalizowanych i uciskanych społeczności w obliczu przeciwności losu było niezachwiane.',
+      'practiceHistoryText3': 'Przez dziesięciolecia "Liberty Legal Solutions" nadal ewoluowało, dostosowując się do zmieniającego się otoczenia prawnego, pozostając jednocześnie wiernym swoim założycielskim zasadom. Prawnicy firmy zajmowali się przełomowymi sprawami, które ukształtowały precedensy prawne i wpłynęły na postęp społeczny.',
+      'practiceHistoryText4': 'Dziś "Liberty Legal Solutions" jest szanowaną instytucją, której dziedzictwem jest obrona swobód obywatelskich, wspieranie sprawiedliwości społecznej i dostarczanie rozwiązań prawnych, które mają trwały wpływ na społeczność, której służy. Pozostaje zaangażowana w swoją historyczną misję, jaką jest obrona wolności i sprawiedliwości dla wszystkich.',
+      '100yearsExperience': 'Ponad 100 lat doświadczenia',
+      'historyDate': 'Historia "Liberty Legal Solutions"',
+      '1920': 'Powstała kancelaria prawna "Liberty Legal Solutions"',
+      '1945': '"Liberty Legal Solutions" po raz pierwszy podejmuje się obrony grupy działaczy na rzecz praw obywatelskich',
+      '1980': 'Kancelaria poszerza obszar swojej praktyki o prawo pracy, prawo ochrony środowiska i ochronę konsumentów.',
+      '2000': 'Tworzy dział pro bono zajmujący się świadczeniem pomocy prawnej społecznościom znajdującym się w niekorzystnej sytuacji.',
+      '2010': 'Firma obchodzi 90. rocznicę swojego istnienia i potwierdza swoje zaangażowanie w przestrzeganie zasad sprawiedliwości i wolności.',
+      'ourTeam': 'Nasz Zespół',
+      'William': 'William Harrison jest doświadczonym prawnikiem. Dzięki ponad trzydziestoletniemu doświadczeniu w dziedzinie prawa jest szanowaną postacią w środowisku prawniczym i kluczową siłą napędową sukcesu firmy. Pan Harrison specjalizuje się w prawie dotyczącym praw obywatelskich, prawie konstytucyjnym i skomplikowanych sporach sądowych. Jego dogłębna wiedza w tych obszarach zapewniła mu reputację potężnego orędownika sprawiedliwości i swobód obywatelskich. Z sukcesem reprezentował klientów w głośnych sprawach, które wywarły trwały wpływ na precedensy prawne.',
+      'Emily': 'Emily Sinclair jest niezwykle utalentowaną prawniczką i pełni funkcję partnera zarządzającego „Liberty Legal Solutions”. Dzięki swojej ponad dwudziestoletniej karierze zyskała reputację prawnika i pioniera w tej dziedzinie. Pani Sinclair specjalizuje się w prawie rodzinnym, szczególnie w sprawach dotyczących skomplikowanych postępowań rozwodowych, sporów o opiekę nad dziećmi oraz negocjacji w sprawie alimentów dla małżonków. Jej rozległe doświadczenie i głębokie zrozumienie dynamiki rodziny sprawiają, że jest poszukiwanym adwokatem swoich klientów.',
+      'offer': 'Oferta',
+      'services': 'Usługi',
+      'laborLaw': 'prawo pracy',
+      'laborLawText': 'Stoisz przed wyzwaniami związanymi z pracą lub szukasz fachowej porady w sprawach związanych z zatrudnieniem? „Liberty Legal Solutions” jest tutaj, aby Ci pomóc. Przez ograniczony czas oferujemy bezpłatne i poufne konsultacje z naszymi doświadczonymi prawnikami z zakresu prawa pracy. Nasz zespół specjalizuje się w umowach o pracę, dyskryminacji w miejscu pracy, sporach płacowych, bezprawnym rozwiązaniu umowy o pracę i nie tylko. Zależy nam na szybkim i skutecznym rozwiązaniu problemu, przy jednoczesnym przestrzeganiu Twoich praw.',
+      'civilLaw': 'prawo cywilne',
+      'civilLawText': 'Czy stoisz przed wyzwaniami związanymi z prawem cywilnym i szukasz porad ekspertów? "Liberty Legal Solutions" jest tutaj, aby Ci pomóc. Przez ograniczony czas mamy przyjemność zaoferować bezpłatne, poufne konsultacje z naszymi doświadczonymi prawnikami z zakresu prawa cywilnego. Nasz zespół specjalizuje się w szerokiej gamie spraw z zakresu prawa cywilnego, w tym w sprawach dotyczących szkód na osobie, sporach kontraktowych, sporach majątkowych i nie tylko. Naszym celem jest dostarczanie spersonalizowanych rozwiązań prawnych, które odpowiadają Twoim unikalnym potrzebom i celom.',
+      'administrativeLaw': 'prawo administracyjne',
+      'administrativeLawText': 'Czy stoisz przed wyzwaniami administracyjno-prawnymi i szukasz porad ekspertów? "Liberty Legal Solutions" jest tutaj, aby Ci pomóc. Przez ograniczony czas mamy przyjemność zaoferować bezpłatne, poufne konsultacje z naszymi doświadczonymi prawnikami z zakresu prawa administracyjnego. Nasz zespół specjalizuje się w różnych kwestiach z zakresu prawa administracyjnego, w tym w zakresie zgodności z przepisami, interakcji z agencjami rządowymi i przesłuchań administracyjnych. Naszym celem jest dostarczanie spersonalizowanych rozwiązań prawnych, które pomogą Ci poradzić sobie ze złożonością procesów administracyjnych.',
+      'carAccidents': 'Wypadki samochodowe',
+      'motorcycleAccidents': 'Wypadki motocyklowe',
+      'oilRigInjury': 'Uraz platformy wiertniczej',
+      'slipAndFall': 'Upadki',
+      'constructionAccidents': 'Wypadki budowlane',
+      'uberAccidents': 'Wypadki Ubera',
+      'workInjuries': 'Urazy w pracy',
+      'bicycleAccidents': 'Wypadki na rowerze','opinion1': 'W czerwcu miałem straszny wypadek samochodowy. Znalazłem Liberty Legal Solutions i skontaktowałem się z Emily. Pomogła mi w znalezieniu i otrzymaniu leczenia, dzięki któremu mogłem wrócić do pracy. Nie mogłam sobie wyobrazić lepszego adwokata w tej sytuacji.',
+      'opinion1': 'Sprawa wypadku samochodowego',
+      'opinion1Text': 'W czerwcu miałem straszny wypadek samochodowy. Znalazłem Liberty Legal Solutions. Pomogła mi w znalezieniu i otrzymaniu leczenia, dzięki któremu mogłem wrócić do pracy. Nie mogłam sobie wyobrazić lepszego adwokata w tej sytuacji.',
+      'opinion2': 'Sprawa o alimenty',
+      'opinion2Text': 'Liberty Legal Solutions wykonało w mojej sprawie niesamowitą robotę. Rozwiązali mój problem rodzinny i umożliwili mi cotygodniowe widywanie się z dziećmi. Gorąco polecam tę kancelarię w każdej sytuacji z zakresu prawa rodzinnego.',
+      'opinion3': 'Sprawa upadku',
+      'opinion3Text': 'Jeśli mieszkasz w Dallas i potrzebujesz prawnika zajmującego się szkodami na osobie, nie mogę bardziej polecić Liberty Legal Solutions! Profesjonalny, komunikatywny i pracowity. Nigdy nie spodziewałem się, że będę mógł czuć się tak zaopiekowany i wysłuchany przez prawnika!',
+      'contactText': 'Internet nie odpowie na wszystkie Twoje pytania. Zamiast tego zwróć się do ekspertów. Skontaktuj się z naszym zespołem, aby porozmawiać z jednym z naszych prawników i dowiedzieć się, jak możemy Ci pomóc.',
+      'contactButton': 'Skontaktuj się z nami',
+      'week': 'Poniedziałek - Piątek:',
+      'hours': '9:00 - 17:00',
+      'weekend': 'Sobota, Niedziela: ',
+      'closed': 'Zamknięte',
+      'questions': 'Jakieś pytania?',
+      'send': 'wyślij',
   }
-  
-  window.addEventListener("scroll", reveal);
-  
+};
+
+function setLanguage(lang) {
+  for (var key in translations[lang]) {
+    var elements = document.querySelectorAll('[data-translate="' + key + '"]');
+      for (var i = 0; i < elements.length; i++) {
+        elements[i].innerText = translations[lang][key];
+      }
+  }
+  localStorage.setItem('selectedLanguage', lang);
+}
+
+const englishButton = document.getElementById('englishButton');
+const polishButton = document.getElementById('polishButton');
+
+englishButton.addEventListener('click', () => {
+  setLanguage('en');
+});
+
+polishButton.addEventListener('click', () => {
+  setLanguage('pl');
+});
+
+var savedLanguage = localStorage.getItem('selectedLanguage') || 'en';
+setLanguage(savedLanguage);
